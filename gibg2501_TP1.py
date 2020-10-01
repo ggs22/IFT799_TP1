65db079828e5c8072d983670458b36df2a5d716f
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-10-01 13:00:46 -0400

    remise

--- gibg2501_TP1.py
+++ gibg2501_TP1.py
@@ -1,3 +1,9 @@
+'''
+Gabriel Gibeau Sanchez - gibg2501
+IFT799 - TP1
+30 sept. 2020
+'''
+
 from sklearn.datasets import load_iris
 from scipy.spatial import distance
 from sklearn.decomposition import PCA
@@ -11,14 +17,19 @@ import matplotlib.pyplot as plt
 # Load iris dataset
 iris_dataset = load_iris()
 
-def get_inter_distances(class_data: list, class_stats: list, method='euclidian', covar_method='classwise'):
 
+def get_inter_distances(class_data: list,
+                        class_stats: list,
+                        covar_data=None,
+                        method='euclidian',
+                        covar_method='classwise'):
     d = list()
     r = dict()
 
     # Get alll directional inter-classes distances with specified the norm
-    if covar_method == 'overall':
-        iv = np.linalg.inv(np.cov(iris.iloc[:, 0:4], rowvar=False))
+    if covar_method == 'overall' and covar_data is not None:
+        # iv = np.linalg.inv(np.cov(iris.iloc[:, 0:4], rowvar=False))
+        iv = np.linalg.inv(np.cov(covar_data, rowvar=False))
 
     for i in range(0, len(iris_dataset['target_names'])):
         remaining_classes = [*range(0, len(iris_dataset['target_names']))]
@@ -27,43 +38,47 @@ def get_inter_distances(class_data: list, class_stats: list, method='euclidian',
             for j in remaining_classes:
                 d.clear()
                 for _, row in class_data[i].iterrows():
-                    d.append(distance.euclidean(row[0:4], class_stats[j].loc['mean']))
+                    d.append(distance.euclidean(row, class_stats[j].loc['mean']))
                 r[iris_dataset['target_names'][j] + '-' + iris_dataset['target_names'][i]] = np.min(d)
         elif method == 'mahalanobis':
             if covar_method == 'classwise':
-                iv = np.linalg.inv(np.cov(class_data[i].iloc[:, 0:4], rowvar=False))
+                iv = np.linalg.inv(np.cov(class_data[i], rowvar=False))
             for j in remaining_classes:
                 d.clear()
                 for _, row in class_data[i].iterrows():
-                    d.append(distance.mahalanobis(row[0:4], class_stats[j].loc['mean'], iv))
+                    d.append(distance.mahalanobis(row, class_stats[j].loc['mean'], iv))
                 r[iris_dataset['target_names'][j] + '-' + iris_dataset['target_names'][i]] = np.min(d)
 
     return r
 
 
-def get_intra_distances(class_data: pd.DataFrame, class_stats: pd.Series, method='euclidian', covar_method='classwise'):
-
+def get_intra_distances(class_data: pd.DataFrame,
+                        class_stats: pd.Series,
+                        covar_data=None,
+                        method='euclidian',
+                        covar_method='classwise'):
     d = list()
     r = dict()
 
-    if covar_method == 'overall':
-        iv = np.linalg.inv(np.cov(iris.iloc[:, 0:4], rowvar=False))
+    if covar_method == 'overall' and covar_data is not None:
+        iv = np.linalg.inv(np.cov(covar_data, rowvar=False))
 
     for i in range(0, len(iris_dataset['target_names'])):
-        numeric_data = class_data[i].iloc[:, 0:4]
+        numeric_data = class_data[i]
         d.clear()
         if method == 'euclidian':
             for _, row in numeric_data.iterrows():
                 d.append(distance.euclidean(row, class_stats[i].loc['mean']))
         elif method == 'mahalanobis':
             if covar_method == 'classwise':
-                iv = np.linalg.inv(np.cov(class_data[i].iloc[:, 0:4], rowvar=False))
+                iv = np.linalg.inv(np.cov(class_data[i], rowvar=False))
             for _, row in numeric_data.iterrows():
                 d.append(distance.mahalanobis(row, class_stats[i].loc['mean'], iv))
 
         r[iris_dataset['target_names'][i]] = np.max(d)
 
     return r
+
 
 # Put dataset in a panda Dataframe
 targets = np.zeros((iris_dataset['data'].shape[0], 1))
@@ -77,7 +92,7 @@ cols.append('target')
 iris = pd.DataFrame(np.concatenate((data, targets), axis=1), columns=cols)
 
 for i in range(0, len(iris_dataset['target_names'])):
-    iris.loc[iris['target'] == i,'target'] = [iris_dataset['target_names'][i]] * np.sum(iris['target'] == i)
+    iris.loc[iris['target'] == i, 'target'] = [iris_dataset['target_names'][i]] * np.sum(iris['target'] == i)
 
 # Part 1
 # Descriptive statistics
@@ -90,7 +105,6 @@ class_stats = list()
 intra_classs_distances = list()
 
 for i in range(0, len(iris_dataset['target_names'])):
-
     unique_class = iris[iris['target'] == iris_dataset['target_names'][i]]
 
     class_data.append(unique_class)
@@ -101,14 +115,31 @@ for i in range(0, len(iris_dataset['target_names'])):
     class_stats.append(class_description)
 
 # get intra-class distances
-intra_dist_euclid = get_intra_distances(class_data, class_stats)
-intra_dist_mahalanobis = get_intra_distances(class_data, class_stats, method='mahalanobis')
-intra_dist_mahalanobis_all = get_intra_distances(class_data, class_stats, method='mahalanobis', covar_method='overall')
+nb_features_kept = 3
+data_arg = [c.drop(columns=iris_dataset['feature_names'][nb_features_kept], axis=1) for c in class_data]
+data_arg = [c.drop(columns='target', axis=1) for c in data_arg]
+stats_arg = [c.drop(columns=iris_dataset['feature_names'][nb_features_kept], axis=1) for c in class_stats]
+covar_data = iris.drop(columns=[iris_dataset['feature_names'][nb_features_kept], 'target'], axis=1)
+
+intra_dist_euclid = get_intra_distances(data_arg, stats_arg)
+intra_dist_mahalanobis = get_intra_distances(data_arg, stats_arg,
+                                             covar_data=covar_data,
+                                             method='mahalanobis')
+intra_dist_mahalanobis_all = get_intra_distances(data_arg, stats_arg,
+                                                 covar_data=covar_data,
+                                                 method='mahalanobis',
+                                                 covar_method='overall')
 
 # get inter-class distances
-inter_dist_euclid = get_inter_distances(class_data, class_stats)
-inter_dist_mahalanobis = get_inter_distances(class_data, class_stats, method='mahalanobis')
-inter_dist_mahalanobis_all = get_intra_distances(class_data, class_stats, method='mahalanobis', covar_method='overall')
+inter_dist_euclid = get_inter_distances(data_arg, stats_arg)
+inter_dist_mahalanobis = get_inter_distances(data_arg, stats_arg,
+                                             covar_data=covar_data,
+                                             method='mahalanobis')
+inter_dist_mahalanobis_all = get_inter_distances(data_arg, stats_arg,
+                                                 covar_data=covar_data,
+                                                 method='mahalanobis',
+                                                 covar_method='overall')
+
 # verify if the classes are well separated
 # if intra_dist_x < inter_dist_(y->x) => x & y are well separated
 print('\nInter & intra-class distances using euclidian norm')
@@ -118,7 +149,7 @@ for i in range(0, len(iris_dataset['target_names'])):
         cname2 = iris_dataset['target_names'][j]
         if cname1 != cname2:
             if inter_dist_euclid[f'{cname1}-{cname2}'] > intra_dist_euclid[cname2]:
-                print(f'{cname1} and {cname2} are well separated!')
+                print(f'{cname1} and {cname2} are well separated.')
             else:
                 print(f'{cname1} and {cname2} are overlaping!')
 
@@ -140,26 +171,26 @@ for i in range(0, len(iris_dataset['target_names'])):
         cname2 = iris_dataset['target_names'][j]
         if cname1 != cname2:
             if inter_dist_mahalanobis[f'{cname1}-{cname2}'] > intra_dist_mahalanobis[cname2]:
-                print(f'{cname1} and {cname2} are well separated!')
+                print(f'{cname1} and {cname2} are well separated.')
             else:
                 print(f'{cname1} and {cname2} are overlaping!')
 
+print('\n')
 
 # Correlation between the features
 print(iris.corr(method='pearson'))
 
 # pairplot plots
-ax = sns.pairplot(iris, hue='target')
-ax._legend.remove()
-plt.legend(iris_dataset['target_names'], loc=4, bbox_to_anchor=(0.9, 0.0))
-plt.savefig('iris pairplot')
-plt.show()
+# ax = sns.pairplot(iris, hue='target')
+# ax._legend.remove()
+# plt.legend(iris_dataset['target_names'], loc=4, bbox_to_anchor=(0.9, 0.0))
+# plt.savefig('iris pairplot')
+# plt.show()
 
 # correlation plot
 sns.heatmap(iris.corr(method='pearson'), cmap='coolwarm', annot=True)
 plt.savefig('iris correlation')
 plt.show()
-
 
 scaler = StandardScaler()
 
@@ -171,15 +202,15 @@ labels = iris['target']
 
 iris_pca = PCA(n_components=3)
 iris_pca.fit(features, labels)
-pcomponents = pd.DataFrame(iris_pca.transform(data))
+iris_tf = iris_pca.transform(data)
+pcomponents = pd.DataFrame(iris_tf)
 
-sns.scatterplot(pcomponents.iloc[:, 0], pcomponents.iloc[:, 1], hue=labels)
-plt.show()
-
-sns.scatterplot(pcomponents.iloc[:, 0], pcomponents.iloc[:, 2], hue=labels)
-plt.show()
-
-sns.scatterplot(pcomponents.iloc[:, 1], pcomponents.iloc[:, 2], hue=labels)
-plt.show()
-
-input("press any key to exit...")
+for i in range(3):
+    for j in range(3):
+        if j > i:
+            sns.scatterplot(pcomponents.iloc[:, i], pcomponents.iloc[:, j], hue=labels, cmap='Set1')
+            plt.xlabel(f'compoosante principale {i}')
+            plt.ylabel(f'compoosante principale {j}')
+            plt.title(f'Nuages de points des \ncompoosantes principales {i} et {j}')
+            plt.savefig(f'Nuages de points des \ncompoosantes principales {i} et {j}')
+            plt.show()
